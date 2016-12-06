870c82ee4a6d6d35be78a53c53de9cc36d153eca
WangPengfei <wangpengfei@engoo.cn>
2016-12-06 09:19:03 +0800

    ...

---
+++ LearnAngularjs/090指令/070表单验证/index01.js
@@ -0,0 +1,8 @@
+/**
+ * Created by w1036_000 on 2016/12/5.
+ */
+var app=angular.module("myApp",[]);
+app.controller('myController',function ($scope) {
+    $scope.$watch()
+
+});
---
+++ LearnAngularjs/090指令/080自定义验证/index01.js
@@ -0,0 +1,7 @@
+/**
+ * Created by w1036_000 on 2016/12/5.
+ */
+var app=angular.module("myApp",[]);
+app.controller('myController',function ($scope) {
+
+});
