8a2e898942a54dab2bae041b0e515967814e6343
WangPengfei <wangpengfei@engoo.cn>
2017-07-04 18:25:29 +0800

    ...

---
+++ LearnAngularjs/动画/index01.js
@@ -0,0 +1,23 @@
+/**
+ * Created by w1036_000 on 2016/12/5.
+ */
+var app = angular.module("myApp", ['ngAnimate']);
+app.controller('myController', function ($scope) {
+
+    /*
+     * CSS基础动画
+     * 适用于ngIf
+     * 定义css：.ng-enter定义进入的开始状态和过渡；.ng-enter.ng-enter-active定义进入的结束状态；.ng-leave定义离开的开始状态和过渡；.ng-leave.ng-leave-active定义离开的结束状态
+     * 可以在开始状态中定义动画
+     * 也可以过渡和动画混合使用
+     * */
+
+    /**
+     * CSS类基础动画
+     * ngHide,ngShow自带动画类：.ng-hide
+     * ngClass使用添加的类名定义动画：.xxx-add .xxx-remove
+     * 动画定义方式与上面类似
+     */
+
+
+});
