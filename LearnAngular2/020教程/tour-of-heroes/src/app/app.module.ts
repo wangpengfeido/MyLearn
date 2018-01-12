8188caa2523d69a2e4b9a876a9df105276dba8e9
Wangpengfei <1036793130@qq.com>
2018-01-12 19:59:30 +0800

    ...

---
+++ LearnAngular2/020教程/tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import {AppComponent} from './app.component';
+import {HeroesComponent} from "./heroes.component";
+import {HeroDetailComponent} from './hero-detail.component';
+
+import {HeroService} from "./hero.service";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
