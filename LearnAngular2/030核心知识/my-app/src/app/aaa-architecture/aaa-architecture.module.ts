09f109989ce471c58d4d71a0268ca6f9eec3479a
wangpengfeido <wangpengfeido@126.com>
2018-03-07 21:46:27 +0800

    ...

--- LearnAngular2/030核心知识/my-app/src/app/aaa-architecture/aaa-architecture.module.ts
+++ LearnAngular2/030核心知识/my-app/src/app/aaa-architecture/aaa-architecture.module.ts
@@ -4,13 +4,16 @@ import {AbaModulesComponent} from './aba-modules.component';
 import { AcaComponentsComponent } from './aca-components.component';
 
 @NgModule({
+  //导入其他模块
   imports: [
     CommonModule
   ],
+  //声明本模块中拥有的视图类。Angular 有三种视图类：组件、指令和管道。
   declarations: [
     AbaModulesComponent,
     AcaComponentsComponent,
   ],
+  //declarations 的子集，可用于其它模块的组件模板
   exports: [AbaModulesComponent]
 })
 export class AaaArchitectureModule {
