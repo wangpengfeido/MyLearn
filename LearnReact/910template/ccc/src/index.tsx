e30c2013327530988f1b9466fbe2aae8d87eaf14
wangpengfeido <wangpengfeido@126.com>
2020-11-26 23:36:09 +0800

    ...

--- LearnReact/910template/ccc/src/index.tsx
+++ LearnReact/910template/ccc/src/index.tsx
@@ -1,29 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './style/index.scss';
-import { App } from './App';
+import "./style/index.scss";
+import { App } from "./App";
 
-const render = ({ dom }: { dom: Element | Document }) => {
-  console.log('--------', dom);
-  ReactDOM.render(
-    <React.StrictMode>
-      <App></App>
-    </React.StrictMode>,
-    dom.querySelector('#root')
-  );
-};
-
-export const provider = () => {
-  return {
-    render,
-    destroy({ dom }: { dom: Element | DocumentFragment }) {
-      ReactDOM.unmountComponentAtNode(dom);
-    },
-  };
-};
-
-// 这能够让子应用独立运行起来
-if (!(window as any).__GARFISH__) {
-  render({ dom: document });
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App></App>
+  </React.StrictMode>,
+  document.querySelector("#root")
+);
